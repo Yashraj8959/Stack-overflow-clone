09edbce0d41664678dc6ea9242bde4d6fe5d360f
Yashraj kushwah <ykushwah7828@gmail.com>
2024-03-16 00:23:52 +0530

    changes in Left sidebar

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import './LeftSidebar.css'
 import { NavLink } from 'react-router-dom'
 import Globe from '../../assets/Globe.svg'
+import homeicon from '../../assets/homeicon.png'
+import questionsicon from '../../assets/questionsicon.png'
+import tagsicon from '../../assets/tagsicon.png'
+import usersicon from '../../assets/usersicon.png'
+
 const LeftSidebar = () => {
   return (
     <div className='left-sidebar'>
         <nav className='side-nav'>
-            <NavLink to="/" className="side-nav-links" activeClassName="active">
-            <p>Home</p>    
-            </NavLink>
             <div className='side-nav-div'>
-                <div>PUBLIC</div>
+            <NavLink to="/" className="side-nav-links" activeClassName="active">
+            <img src={homeicon} alt="home" style={{width:"18px", height:"18px"}} />
+            <p style={{paddingLeft:"10px"}}>Home</p>    
+            </NavLink>
                 <NavLink to="/Questions" className="side-nav-links" activeClassName="active">
-                    <img src={Globe} alt="globe" />
+                    <img src={questionsicon} alt="questions" style={{width:"18px", height:"18px"}} />
                     <p style={{paddingLeft:"10px"}}>Questions</p>
                 </NavLink>
                 <NavLink exact to="/Tags" className="side-nav-links" activeClassName="active">
-                    <p>Tags</p> 
+                <img src={tagsicon} alt="tags" style={{width:"18px", height:"18px"}} />
+                    <p style={{paddingLeft:"10px"}}>Tags</p> 
                 </NavLink>
                 <NavLink exact to="/Users" className="side-nav-links" activeClassName="active">
-                   <p>Users</p>
+                <img src={usersicon} alt="home" style={{width:"18px", height:"18px"}} />
+                   <p style={{paddingLeft:"10px"}}>Users</p>
                 </NavLink>
             </div>
 
