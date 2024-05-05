e1ce201e4491c9f4808a204e664fd87bf6f86b2a
Yashraj kushwah <ykushwah7828@gmail.com>
2024-05-05 23:52:23 +0530

    updating API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stack-overflow-clone-0qd7.onrender.com",
+  baseURL: "https://stack-overflow-production-95u6.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
